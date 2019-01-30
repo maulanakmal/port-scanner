9dee02954cd561dcff51050cd4dc80f1c95d0ecf
mol <maulana2904@gmail.com>
2019-01-30 19:07:54 +0700

    Implement port scanner

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[]) {
 
@@ -11,4 +13,30 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
+    char* IPv4 = argv[1];
+    int port_start = atoi(argv[2]);
+    int port_end = atoi(argv[3]);
+    
+    int socket_fd;
+    struct sockaddr_in socket_address;
+    int status;
+
+    memset(&socket_address, 0, sizeof(struct sockaddr_in));
+    socket_address.sin_family = AF_INET;
+    if (inet_pton(AF_INET, IPv4, &socket_address.sin_addr) <= 0) {
+        printf("invalid addr\n");
+        exit(1);
+    }
+
+
+    for (int port = port_start; port <= port_end; ++port) {
+        if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+            printf("error creating socket\n");
+            exit(1);
+        }
+        socket_address.sin_port = htons(port);
+        status = connect(socket_fd, (struct sockaddr*)&socket_address, sizeof(socket_address));
+        printf("scanning port %d: open\n");
+        close(socket_fd);
+    }
 }
